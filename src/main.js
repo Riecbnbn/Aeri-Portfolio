e0285d6c089355d9370a2c8768f236f71be944c3
Riecbnbn <ejanecabanban@gmail.com>
2025-06-09 11:26:52 +0800

    updated codes

--- src/main.js
+++ src/main.js
@@ -5,9 +5,9 @@ import router from './router';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import { faUser, faImage, faEnvelope, faPaintBrush, faBars } from '@fortawesome/free-solid-svg-icons';
-import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons' // Import these
+import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons' 
 library.add(faUser, faImage, faEnvelope, faPaintBrush, faBars);
-library.add(faChevronLeft, faChevronRight) // Add them to the library
+library.add(faChevronLeft, faChevronRight) 
 const app = createApp(App);
 app.component('font-awesome-icon', FontAwesomeIcon);
 app.use(router).mount('#app');
